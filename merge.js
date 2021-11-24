bf6ff7cf2eb4ebe1c776b719935af00c4d1ff91d
Mark Smithson <mark@smithson.me>
2021-11-24 06:25:09 +0000

    fix: do not merge allowlists
    this does not work as expected as rules are applied using AND and not OR

--- merge.js
+++ merge.js
@@ -2,24 +2,6 @@ const process = require('process');
 const fs = require('fs');
 const TOML = require('@iarna/toml');
 
-const mergeAllowList = (base, overrides) => {
-  if (!base.allowlist){
-    return overrides.allowlist;
-  }
-  const result = base.allowlist;
-  if (! overrides.allowlist){
-    return result;
-  }
-  const props = ["commits", "files", "paths", "regexes"];
-  props.forEach(prop => {
-    result[prop] = [...(result[prop] || []), ...(overrides.allowlist[prop] || [])];  
-    if (result[prop].length === 0){
-      delete result[prop];
-    }
-  });
-  return result;
-}
-
 if (process.argv.length !== 4) {
   console.error("Usage: merge base.toml overrides.tml");
   process.exit(1);
@@ -31,13 +13,13 @@ const overridesObject = TOML.parse(fs.readFileSync(process.argv[3], 'utf8'));
 overridesObject.rules.forEach(rule => {
   const baseRule = baseObject.rules.find(_rule => _rule.description === rule.description);
   if (baseRule) {
-    baseRule.allowlist = mergeAllowList(baseRule, rule)
+    baseRule.allowlist = rule.allowlist
   }
   else {
     baseObject.rules.push(rule);
   }
 });
 
-baseObject.allowlist = mergeAllowList(baseObject, overridesObject);
+baseObject.allowlist = overridesObject.allowlist || baseObject.allowlist;
 
 console.log(TOML.stringify(baseObject));
